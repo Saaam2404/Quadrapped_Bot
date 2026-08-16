2d14f46eb619c97d667021743b0b84b5067d68f2
Sam2404 <samarpan1645@gmail.com>
2026-08-16 12:41:18 +0530

    Added

--- pet_bot_test5/scripts/backward_gait.py
+++ pet_bot_test5/scripts/backward_gait.py
@@ -57,7 +57,7 @@ def leg_ik(x, z):
 def foot_position(s, swing):
     if swing:
         x = DIRECTION * (-STEP_LENGTH / 2 + STEP_LENGTH * s)
-        z = BODY_HEIGHT - STEP_HEIGHT * math.sin(math.pi * s)
+        z = BODY_HEIGHT - STEP_HEIGHT * (math.sin(math.pi * s))**2
     else:
         x = DIRECTION * (STEP_LENGTH / 2)
         z = BODY_HEIGHT
